c26548bfc45aef4441e42f7d04639b87eea266e0
Jacob Nelson <jnelson@bgc-jena.mpg.de>
2018-02-26 20:14:16 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='TranspirationEstimationAlgorithm',
+      version='0.02',
+      description='Water Flux Partitioning',
+      author='Jacob A. Nelson',
+      author_email='jnelson@bgc-jena.mpg.de',
+      url='https://github.com/jnelson18/TranspirationEstimationAlgorithm',
+     )
