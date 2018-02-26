9c137ee95e22005b58e254b59f21df2b0ae899a4
Jacob Nelson <jnelson@bgc-jena.mpg.de>
2018-02-26 19:23:43 +0100

    Update of supporting files and tutorial

--- TEA.py
+++ TEA.py
@@ -2,18 +2,81 @@
 """
 Created on Mon May 29 15:54:06 2017
 
-@author: jnelson
+@author: jTEA
 """
 
 import numpy as np
 import os
 import datetime
+import multiprocessing as mp
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import Imputer
-from JMaths.Regs import RFPercentilePrediction
 
-def partition(ds,percs=np.linspace(20,60,5),lims=np.array([-1]),n_jobs=47,RFmod_vars=['Rg','Tair','RH','u','month','year','GPPgrad','DWCI','C_Rg_LE','MWB']):
+class RFP:
+    def __init__(self, trainRFy, TreeNodeIDXTrain,TreeNodeIDXPred,pcts):
+        self.trainRFy=trainRFy
+        self.TreeNodeIDXTrain=TreeNodeIDXTrain
+        self.TreeNodeIDXPred=TreeNodeIDXPred
+        self.pcts=pcts
+    def Calc_pcts(self,i):
+        return(np.percentile(self.trainRFy[np.where(self.TreeNodeIDXTrain==self.TreeNodeIDXPred[i,:])[0]],self.pcts))
+
+
+def RFPercentilePrediction(Forest,trainRFxs,trainRFy,predRFxs,pcts=[5,50,95],n_jobs=1,maxtasksperchild=500):
+    """RFQuantilePrediction(Forest,trainRFxs,trainRFy,predRFxs,pcts=[5,50,95])
+
+    Random Forest Percentile Prediction
+
+    Fits a slope between x and y individually with an intercept of zero, then takes the geometric mean of the slopes.
+
+
+    Parameters
+    ----------
+    Forest : class 'sklearn.ensemble.forest.RandomForestRegressor'
+        Fitted random forest regressor from sklearn
+    trainRFxs : numpy array
+        The x dataset used to fit Forest
+    trainRFy : numpy array
+        The y dataset used to fit Forest
+    predRFxs : numpy array
+        The x dataset to get the percentile prediction
+    pcts : list or list like
+        The percentiles to output, defaults are the 5th, 50th, and 95th percentiles
+    n_jobs:
+        Passed to multiprocessing.Pool number of processors to use.
+    maxtasksperchild : integer
+        Passed to multiprocessing.Pool job tasts to complete before being cleaned, to save memory.
+
+     Returns
+    -------
+    numpy ndarray
+        The resulting percentile prediction shaped [npred,npcts]
+    """
+
+    ntrees=Forest.n_estimators
+    n=trainRFy.shape[0]
+    TreeNodeIDXTrain=np.zeros([n,ntrees])
+    npred=predRFxs.shape[0]
+    TreeNodeIDXPred=np.zeros([npred,ntrees])
+
+
+    for i in range(ntrees):
+        TreeNodeIDXTrain[:,i]=Forest.estimators_[i].apply(trainRFxs)
+        TreeNodeIDXPred[:,i]=Forest.estimators_[i].apply(predRFxs)
+
+    ypred_pcts=np.ones([npred,len(pcts)])*np.nan
+
+    c=RFP(trainRFy, TreeNodeIDXTrain,TreeNodeIDXPred,pcts)
+
+    if n_jobs>1:
+        with mp.Pool(processes=n_jobs,maxtasksperchild=maxtasksperchild) as p:
+            ypred_pcts=np.array(list(p.map(c.Calc_pcts, range(npred))))
+    else:
+        ypred_pcts=np.array(list(map(c.Calc_pcts, range(npred))))
+
+    return(ypred_pcts)
+
+def partition(ds,percs=np.linspace(50,100,11),CSWIlims=np.array([-1]),n_jobs=1,RFmod_vars=['Rg','Tair','RH','u','Rg_pot_daily','Rgpotgrad','year','GPPgrad','DWCI','C_Rg_ET','CSWI']):
     RFxs=[ds[x].values for x in RFmod_vars]
     RFxs=np.matrix(RFxs).T
     RFxs[np.isnan(RFxs)]=-9999
@@ -22,44 +85,48 @@ def partition(ds,percs=np.linspace(20,60,5),lims=np.array([-1]),n_jobs=47,RFmod_
     ds.attrs['features']=','.join(RFmod_vars)
     
     ds.coords['percentiles']=percs
-    ds.coords['MWBlims']=lims
+    ds.coords['CSWIlims']=CSWIlims
     ds.coords['RFmod_vars']=RFmod_vars
     ds['nanflag']=(('timestamp'),((~np.isnan(ds.ET)) & (~np.isnan(ds.GPP))).astype(bool))
     
-    ds['Nelson_WUE']=(('timestamp','percentiles','MWBlims'),np.zeros((ds.timestamp.size,percs.size,lims.size))-9999)
-    ds['Nelson_T']=(('timestamp','percentiles','MWBlims'),np.zeros((ds.timestamp.size,percs.size,lims.size))-9999)
-    ds['Nelson_E']=(('timestamp','percentiles','MWBlims'),np.zeros((ds.timestamp.size,percs.size,lims.size))-9999)
+    ds['TEA_WUE']=(('timestamp','percentiles','CSWIlims'),np.zeros((ds.timestamp.size,percs.size,CSWIlims.size))-9999)
+    ds['TEA_T']=(('timestamp','percentiles','CSWIlims'),np.zeros((ds.timestamp.size,percs.size,CSWIlims.size))-9999)
+    ds['TEA_E']=(('timestamp','percentiles','CSWIlims'),np.zeros((ds.timestamp.size,percs.size,CSWIlims.size))-9999)
 
-    ds['RFflag']=(('timestamp','MWBlims'),np.zeros((ds.timestamp.size,lims.size))-9999)
-    ds['feature_importances']=(('RFmod_vars','MWBlims'),np.zeros((len(RFmod_vars),lims.size))-9999)
-    ds['NumForestPoints']=(('MWBlims'),np.zeros((lims.size))-9999)
-    ds['oob_scores']=(('MWBlims'),np.zeros((lims.size))-9999)
+    ds['RFflag']=(('timestamp','CSWIlims'),np.zeros((ds.timestamp.size,CSWIlims.size))-9999)
+    ds['feature_importances']=(('RFmod_vars','CSWIlims'),np.zeros((len(RFmod_vars),CSWIlims.size))-9999)
+    ds['NumForestPoints']=(('CSWIlims'),np.zeros((CSWIlims.size))-9999)
+    ds['oob_scores']=(('CSWIlims'),np.zeros((CSWIlims.size))-9999)
 
     for var in RFmod_vars:
         if np.any(np.isnan(ds[var][ ds['nanflag']])) or np.any(ds[var][ ds['nanflag']]<-9000):
             RFmod_vars.remove(var)
 
-    for l in range(lims.size):
-        CurFlag=ds.Baseflag.values*(ds.MWB.values<ds.coords['MWBlims'].values[l])
+    for l in range(CSWIlims.size):
+        CurFlag=ds.Baseflag.values*(ds.CSWI.values<ds.coords['CSWIlims'].values[l])
         ds['RFflag'][:,l]=CurFlag
         ds['NumForestPoints'][l]=CurFlag.sum()
         
         if CurFlag.sum()>240:
             Forest=RandomForestRegressor(n_estimators=100, oob_score=True, n_jobs=n_jobs, verbose=0, warm_start=False)
-            Forest.fit(RFxs[CurFlag],ds.instET_WUE.values[CurFlag])
+            Forest.fit(RFxs[CurFlag],ds.inst_WUE.values[CurFlag])
     
             ds['oob_scores'][l]=Forest.oob_score_
 
             for j in range(len(RFmod_vars)):
                 ds.feature_importances.sel(RFmod_vars=RFmod_vars[j])[l]=Forest.feature_importances_[j]
 
-            ds['Nelson_WUE'][:,:,l][ds['nanflag'].values]=RFPercentilePrediction(Forest,RFxs[CurFlag],
-                                                                ds.instET_WUE.values[CurFlag],
+            ds['TEA_WUE'][:,:,l][ds['nanflag'].values]=RFPercentilePrediction(Forest,RFxs[CurFlag],
+                                                                ds.inst_WUE.values[CurFlag],
                                                                 RFxs[ds['nanflag'].values],
                                                                 ds.percentiles.values,n_jobs=n_jobs)#.flatten()
 
-            ds['Nelson_T'][:,:,l]=ds['Nelson_WUE'][:,:,l]*ds.GPP
-            ds['Nelson_E'][:,:,l]=ds.ET-ds['Nelson_WUE'][:,:,l]    
+            ds['TEA_T'][:,:,l]=ds.GPP/(ds['TEA_WUE'][:,:,l]*(1000/(12*1800)))
+            ds['TEA_E'][:,:,l]=ds.ET-ds['TEA_T'][:,:,l]
+            
+            ds['TEA_T'] = ds['TEA_T'].assign_attrs({'long name':'TEA transpiration','Units':'mm hh-1'})
+            ds['TEA_E'] = ds['TEA_E'].assign_attrs({'long name':'TEA evaporation','Units':'mm hh-1'})
+            ds['TEA_WUE'] = ds['TEA_WUE'].assign_attrs({'long name':'TEA water use efficiency','Units':'g C per kg H2O'})
     
     ds.attrs['PartRunTimestamp']=datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
     try:
--- TranspirationEstimationAlgorithm-master/README.md
+++
@@ -1,2 +0,0 @@
-# TranspirationEstimationAlgorithm
-A method of estimating transpiration directly from carbon, water, and energy fluxes, such as eddy covariance datasets
