381f404b74427a8cecad1c80c0e3fbf0273cf0b9
Jacob A. Nelson <jacobnelson18@gmail.com>
2018-02-27 16:37:02 +0100

    fixed bug in outlierCorrection

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='TranspirationEstimationAlgorithm',
-      version='0.04',
+      version='0.06',
       description='Water Flux Partitioning',
       author='Jacob A. Nelson',
       author_email='jnelson@bgc-jena.mpg.de',
