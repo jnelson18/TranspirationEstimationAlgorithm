46b5101dfadb08d9c321af92deac8c65a88f5052
jnelson <jacobnelson18@gmail.com>
2018-06-22 15:38:47 +0200

    Fix PreProc.umolC_per_s_to_gC_per_d bug

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='TranspirationEstimationAlgorithm',
-      version='v0.10',
+      version='v0.11',
       description='Water Flux Partitioning',
       author='Jacob A. Nelson',
       author_email='jnelson@bgc-jena.mpg.de',
