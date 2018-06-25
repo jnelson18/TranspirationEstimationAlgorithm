9449f19beffe7c20d02bb1c890f9a957294e8f29
jnelson <jacobnelson18@gmail.com>
2018-06-25 13:30:38 +0200

    update GPPdayLim to 1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='TranspirationEstimationAlgorithm',
-      version='v0.11',
+      version='v0.12',
       description='Water Flux Partitioning',
       author='Jacob A. Nelson',
       author_email='jnelson@bgc-jena.mpg.de',
